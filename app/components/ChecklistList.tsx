1556ff3ff85b83be428a5545be483cc2a8e66c19
João Cabrita <kewne@protonmail.com>
2026-05-31 16:06:35 +0000

    refactor: use list component for checklist list

--- app/components/ChecklistList.tsx
+++ app/components/ChecklistList.tsx
@@ -1,4 +1,7 @@
-import { ChecklistItem } from "./ChecklistItem";
+import { encodeApiUrl } from "../lib/encoding";
+import { Button } from "./Button";
+import { Link } from "./Link";
+import { List } from "./List";
 import type { ApiResource } from "~/lib/api";
 
 interface ChecklistListProps {
@@ -9,22 +12,27 @@ interface ChecklistListProps {
 export function ChecklistList({ resource, onDelete }: ChecklistListProps) {
   const items = resource.getLinkArray("items");
 
+  if (items.length === 0) {
+    return (
+      <div className="px-4 py-3 text-gray-500 text-sm">
+        No checklists found.
+      </div>
+    );
+  }
+
   return (
-    <ul
-      aria-label="checklists"
-      className="mt-4 divide-y divide-gray-100 border border-gray-200 bg-white"
-    >
-      {items.length === 0 ? (
-        <li className="px-4 py-3 text-gray-500 text-sm">
-          No checklists found.
-        </li>
-      ) : (
-        items.map((item) => (
-          <li key={item.href}>
-            <ChecklistItem item={item} onDelete={onDelete} />
-          </li>
-        ))
-      )}
-    </ul>
+    <List
+      ariaLabel="checklists"
+      items={items.map((item) => (
+        <>
+          <Link to={`/checklists/show/${encodeApiUrl(item.href)}`}>
+            {item.title ?? item.name}
+          </Link>
+          <Button variant="danger" action={onDelete(item.href)}>
+            Delete
+          </Button>
+        </>
+      ))}
+    />
   );
 }
