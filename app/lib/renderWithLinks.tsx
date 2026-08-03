200bdb0a002ebfc1bb214f18a44a8f05d21b8d3c
João Cabrita <kewne@protonmail.com>
2026-08-03 20:28:26 +0000

    feat: shorten links in descriptions

--- app/lib/renderWithLinks.tsx
+++ app/lib/renderWithLinks.tsx
@@ -4,10 +4,17 @@ export function renderWithLinks(text: string): React.ReactNode[] {
   return text.split(/(\s+)/).map((token, i) => {
     if (/^\s+$/.test(token)) return token;
     const url = URL.parse(token);
-    if (url?.protocol === "https:" || url?.protocol === "mailto:") {
+    if (url?.protocol === "https:") {
       return (
         <Link key={i} to={token} target="_blank">
-          {token}
+          {url.hostname}
+        </Link>
+      );
+    }
+    if (url?.protocol === "mailto:") {
+      return (
+        <Link key={i} to={token} target="_blank">
+          {url.pathname}
         </Link>
       );
     }
